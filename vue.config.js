b04a57fc0152dbb64c066f590368537c90d9e081
Kyle_Hermens <kyle.hermens@gmail.com>
2021-10-24 16:00:19 -0600

    initial commit of vue project template with wasm-pack additions

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const path = require("path");
+
+module.exports = {
+  chainWebpack: config => {
+    // rust wasm bindgen https://github.com/rustwasm/wasm-bindgen
+    config
+      .plugin("wasm-pack")
+      .use(WasmPackPlugin)
+      .init(
+        Plugin =>
+          new Plugin({
+            crateDirectory: path.resolve(__dirname, "./wasm")
+          })
+      )
+      .end()
+  }
+};
